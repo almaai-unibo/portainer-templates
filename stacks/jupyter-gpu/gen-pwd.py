fbd057c414c400c82a828b8fc30538eb10360723
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-12-19 15:31:14 +0100

    fix: gen-pwd should support jupyter 7+

--- stacks/jupyter-gpu/gen-pwd.py
+++ stacks/jupyter-gpu/gen-pwd.py
@@ -1,5 +1,18 @@
-from notebook.auth import passwd
 from sys import argv
+from importlib import import_module
+
+
+def load_passwd(name='passwd', *modules):
+    for module in modules:
+        try:
+            m = import_module(module)
+            return getattr(m, name)
+        except (AttributeError, ModuleNotFoundError):
+            pass
+    raise ImportError(f"Cannot load function '{name}' from any of the following modules: {modules}")
+
+
+passwd = load_passwd("passwd", "notebook.auth", "jupyter_server.auth")
 
 DEFAULT_PATH = "/run/secrets/jupyter_password"
 
